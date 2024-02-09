8a6a2ae49f16ce86e41f21bac18e09ee334a1c9f
Plabpluem <137916501+Plabpluem@users.noreply.github.com>
2024-02-09 16:06:46 +0700

    Update NavbarPage.js

--- src/Ul/NavbarPage.js
+++ src/Ul/NavbarPage.js
@@ -2,8 +2,15 @@ import { Link } from "react-router-dom";
 import classes from "./navbarpage.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
 
 const NavbarPage = (props) => {
+   const dispatch = useDispatch();
+
+  const onSubmitHandler = (e, title, filter) => {
+    dispatch(statusAction.changeFilter({ filter: filter, title: title }));
+    props.closeNavbar
+  };
   return (
     <>
       {props.show && (
@@ -13,28 +20,45 @@ const NavbarPage = (props) => {
             <FontAwesomeIcon icon={faXmark} style={{color: "#ffffff",}} onClick={props.closeNavbar} className={classes.close}/>
           <ul>
             <li>
-              <Link to="/product" onClick={props.closeNavbar}>ALL</Link>
+              <Link to="/product" onClick={(e) => {
+                onSubmitHandler(e, "สินค้าทั้งหมด", [
+                  "Mixednuts",
+                  "CaramelMixnut",
+                  "CaramelCoffee",
+                  "Pistachio",
+                  "Walnuts",
+                  'Macadamia',
+                  "Peacan",
+                  "CaramelMixnut",
+                  "CaramelMacadamia",
+                  "CaramelCoffee",
+                  "Cookiesingapore",
+                  "Cerealcookies",
+                  "Apricotdried",
+                  "Cranberrydried",
+                ]);
+              }}>ALL</Link>
             </li>
             <li>
-              <Link to="/product/productMixednuts" onClick={props.closeNavbar}>Mixed Nuts</Link>
+              <Link to="/product" onClick={props.closeNavbar}>Mixed Nuts</Link>
             </li>
             <li>
-              <Link to="/product/productMacadamia" onClick={props.closeNavbar}>Macadamia</Link>
+              <Link to="/product" onClick={props.closeNavbar}>Macadamia</Link>
             </li>
             <li>
-              <Link to="/product/productPistachios" onClick={props.closeNavbar}>Pistachios</Link>
+              <Link to="/product" onClick={props.closeNavbar}>Pistachios</Link>
             </li>
             <li>
-              <Link to="/product/productWalnuts" onClick={props.closeNavbar}>Walnuts</Link>
+              <Link to="/product" onClick={props.closeNavbar}>Walnuts</Link>
             </li>
             <li>
-              <Link to="/product/productPeacans" onClick={props.closeNavbar}>Peacans</Link>
+              <Link to="/product" onClick={props.closeNavbar}>Peacans</Link>
             </li>
             <li>
-              <Link to="/product/productSweetandcookie" onClick={props.closeNavbar}>Sweet treats</Link>
+              <Link to="/product" onClick={props.closeNavbar}>Sweet treats</Link>
             </li>
             <li>
-              <Link to="/product/productDriedFruits" onClick={props.closeNavbar}>Dried Fruits</Link>
+              <Link to="/product" onClick={props.closeNavbar}>Dried Fruits</Link>
             </li>
           </ul>
         </div>
