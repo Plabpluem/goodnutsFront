ab55beba99d41fca2d17f6aeb2416e3d1f1ed417
Plabpluem <137916501+Plabpluem@users.noreply.github.com>
2024-02-22 02:11:24 +0700

    new slide

--- src/Page/Product/ProductPage.js
+++ src/Page/Product/ProductPage.js
@@ -1,13 +1,14 @@
-import React, { Suspense,useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import "./ProductPage.css";
 import Shopping from "../../component/Shopping/Shopping";
 import NavbarFilter from "../../component/NavbarFilter/NavbarFilter";
 import { Await, defer, useRouteLoaderData } from "react-router-dom";
-import {useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import LoadingPage from "../LoadingPage";
+import axios from "axios";
 
 const ProductPage = () => {
-  const {data} = useRouteLoaderData("product-list");
+  const { data } = useRouteLoaderData("product-list");
   const status = useSelector((state) => state.status);
   const [navbarData, setNavbarData] = useState([
     "Mixednuts",
@@ -44,9 +45,16 @@ const ProductPage = () => {
           changeFilterNavbar={onChangeFilterNavbar}
           changeTitle={onChangeTitle}
         />
-      <Await resolve={data}>
-        {loadevent => <Shopping datalist={loadevent.filter((item) => navbarData.includes(item.type))} title={titleData} />}
-      </Await>
+        <Await resolve={data}>
+          {(loadevent) => (
+            <Shopping
+              datalist={loadevent.filter((item) =>
+                navbarData.includes(item.type)
+              )}
+              title={titleData}
+            />
+          )}
+        </Await>
       </div>
     </Suspense>
   );
@@ -54,24 +62,35 @@ const ProductPage = () => {
 
 export default ProductPage;
 
-const loadData = async() => {
-  const response = await fetch(
-    `${process.env.REACT_APP_GOODNUT_API}/product/product`,
-    {
-      headers: {
-        'Content-Type':'application/json'
-      },
-    }
-  );
-  if (!response.ok) {
-    throw new Error("Loading product Error");
+const loadData = async () => {
+  try {
+    const response = await axios.get(
+      `${process.env.REACT_APP_GOODNUT_API}/product/product`
+    );
+
+    console.log(response)
+    return response.data.products;
+  } catch (error) {
+    console.log(error);
   }
-  const responseData = await response.json();
-  return responseData.products;
-}
+
+  // const response = await fetch(
+  //   `${process.env.REACT_APP_GOODNUT_API}/product/product`,
+  //   {
+  //     headers: {
+  //       'Content-Type':'application/json'
+  //     },
+  //   }
+  // );
+  // if (!response.ok) {
+  //   throw new Error("Loading product Error");
+  // }
+  // const responseData = await response.json();
+  // return responseData.products;
+};
 
 export const loader = async () => {
   return defer({
-    data: loadData()
-  })
+    data: loadData(),
+  });
 };
--- src/component/slideHeader/sliderHeader.js
+++ src/component/slideHeader/sliderHeader.js
@@ -1,8 +1,13 @@
 import classes from "./sideheader.module.css";
+import "./style.css";
 import { useEffect, useState } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const SlideHeader = () => {
-  const PicTItle = ['https://img2.pic.in.th/pic/About534d8572d330f719.jpeg', 'https://img5.pic.in.th/file/secure-sv1/About2.jpeg'];
+  const PicTItle = [
+    "https://img2.pic.in.th/pic/About534d8572d330f719.jpeg",
+    "https://img5.pic.in.th/file/secure-sv1/About2.jpeg",
+  ];
   const [indexPic, setIndexPic] = useState(0);
 
   useEffect(() => {
@@ -15,7 +20,11 @@ const SlideHeader = () => {
 
   return (
     <div className={classes.slider}>
-      <img src={PicTItle[indexPic]} alt="" />
+      <TransitionGroup>
+        <CSSTransition key={indexPic} classNames="fade" timeout={1000}>
+          <img src={PicTItle[indexPic]} alt="" />
+        </CSSTransition>
+      </TransitionGroup>
       <div className={classes.logoBrand}>
         <h1>Goodnuts</h1>
         <p>ถั่วอบทรัฟเฟิล คิดสูตรเอง ขายเอง เจ้าแรก</p>
